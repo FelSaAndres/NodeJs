a348e5d3110100b44fd36b282001ee577dcc23ef
Felipe <felipesardans@gmail.com>
2022-12-30 14:03:15 -0300

    desafio LoggsDebug

--- DesafioENVyProcess/models/usersModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = {
-    username: String,
-    password: String,
-}
-
-const userModel = mongoose.model('usuario', userSchema)
-module.exports = {userModel}
